abeb72b40a006ea7c70988660fc39f43d98f6d0c
Othniel Che <cheothniel@gmail.com>
2025-07-05 13:02:24 +0100

    add

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Use local proxy in development, direct API in production
-// export const API_BASE_URL =  'https://api.algee.co/api'; 
-export const API_BASE_URL =  'http://localhost:3000/api'; 
+export const API_BASE_URL =  'https://api.algee.co/api'; 
+// export const API_BASE_URL =  'http://localhost:3000/api'; 
