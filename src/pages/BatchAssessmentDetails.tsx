e790ce892a83bbcf4859853920cb932efdeffd93
Jeme Beseka F <j.beseka@gmail.com>
2025-07-28 18:04:34 +0100

    /

--- src/pages/BatchAssessmentDetails.tsx
+++ src/pages/BatchAssessmentDetails.tsx
@@ -80,7 +80,7 @@ const BatchAssessmentDetails = () => {
   }
 
   return (
-    <>
+    <Layout>
       <div className="md:flex md:items-center md:justify-between mb-8">
         <div className="flex-1 min-w-0">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
@@ -292,7 +292,7 @@ const BatchAssessmentDetails = () => {
           </div>
         </div>
       )}
-    </>
+    </Layout>
   );
 };
 
